2e438bac0532d66baee0513d042d3a29ac2c58bc
neejalsubedi <subedineejal@gmail.com>
2025-08-20 16:20:27 +0545

    fix

--- server.js
+++ server.js
@@ -8,25 +8,7 @@ const authRoutes = require("./src/routes/authRoutes");
 
 const app = express();
 app.use(express.json());
-const allowedOrigins = [
-  "http://localhost:5173", // local dev frontend
-  "https://shop.primosremit.com.au", // deployed frontend domain
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // important if you send cookies/auth headers
-  })
-);
+app.use(cors({ origin: "*" }));
 
 const PORT = process.env.PORT || 5000;
 
