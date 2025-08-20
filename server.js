09c4709f9c82b316c59edaf5051fbac040ff4da7
neejalsubedi <subedineejal@gmail.com>
2025-08-20 16:13:14 +0545

    fix

--- server.js
+++ server.js
@@ -8,8 +8,25 @@ const authRoutes = require("./src/routes/authRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173", // local dev frontend
+  "https://shop.primosremit.com.au", // deployed frontend domain
+];
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // important if you send cookies/auth headers
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,11 +41,11 @@ mongoose
   })
   .catch((err) => console.error(err));
 
-  // Routes
+// Routes
 app.use("/api/products", require("./src/routes/ProductRoutes"));
 app.use("/api/categories", require("./src/routes/CategoryRoutes"));
 app.use("/api/roles", require("./src/routes/roleRoute"));
 app.use("/uploads", express.static("uploads"));
-app.use('/api/auth', authRoutes);
-app.use('/api/Users',require("./src/routes/UerRoutes"))
-app.use('/api/Orders', require('./src/routes/OrderRoutes'));
+app.use("/api/auth", authRoutes);
+app.use("/api/Users", require("./src/routes/UerRoutes"));
+app.use("/api/Orders", require("./src/routes/OrderRoutes"));
